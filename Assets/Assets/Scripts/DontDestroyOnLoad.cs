ddf51d00d36ebdb04c13cd5bb98bcc26cc6dfe33
Benjamin Huguenin <huguenin.benjamin@gmail.com>
2016-04-08 12:09:32 -0700

    Starting clean map scene.

--- Assets/Assets/Scripts/DontDestroyOnLoad.cs
+++ Assets/Assets/Scripts/DontDestroyOnLoad.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DontDestroyOnLoad : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 }
